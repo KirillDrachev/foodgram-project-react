9c2490f9d60915bed2a7ea8f1f6f74b3a9ff73a4
KirillDrachev <drachev.kirill@yandex.ru>
2023-07-22 20:02:52 +0300

    final3

--- backend/manage.py
+++ backend/manage.py
@@ -9,7 +9,7 @@ def main():
     os.environ.setdefault(
         'DJANGO_SETTINGS_MODULE',
         'foodgram_backend.settings'
-        )
+    )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
